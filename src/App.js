53e8add38f1ce65889496e86b0fb675eeed370f4
hmdev2 <hmdev2@proton.me>
2024-12-02 20:19:54 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [reverse, setReverse] = useState(false);
+  const [counter, setCounter] = useState(0);
+  const reverter = reverse ? 'reverse' : '';
+
+  const handleReverse = () => {
+    setReverse(!reverse);
+  };
+
+  const handleCounter = () => {
+    setCounter(counter + 1);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className={`App-logo ${reverter}`} alt="logo" />
+
+        <h1>Counter: {counter}</h1>
+
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <button type="button" onClick={handleReverse}>
+            Reverse {reverter}
+          </button>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <button type="button" onClick={handleCounter}>
+          Count
+        </button>
       </header>
     </div>
   );
